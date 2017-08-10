e5c7717395047d9636bfc49a8ff76282e11539a5
deeper <deeper@gmail.com>
2017-08-10 15:25:15 +0800

    update

--- LianLianVideo.py
+++ LianLianVideo.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-import requests, json, time,sys
+__author__ = 'limitless'
+
+"""
+    blog : http://www.jianshu.com/u/79a88a044955
+    use:
+    1. python LianLianVideo.py
+    2. 输入url类别(如分类地址  http://v.23c.im/rs 填写 rs)
+    3. 输入url名称(如视频地址 http://v.23c.im/rs148 填写 rs148)
+"""
+import requests, json, time, sys
 
 
 def download(url):
@@ -10,18 +19,18 @@ def download(url):
     dest = "F:/" + name + ".mp4"
 
     requests.adapters.DEFAULT_RETRIES = 5
-    response = requests.get(url, headers = headers,stream=True)
+    response = requests.get(url, headers=headers, stream=True)
     status = response.status_code
     if status == 200:
         total_size = int(response.headers['Content-Length'])
-        print("total_size :" + str(total_size))
         with open(dest, 'wb') as of:
             for chunk in response.iter_content(chunk_size=102400):
                 if chunk:
                     of.write(chunk)
-        print(name + " 下载完成")
+        print("视频：" + name + "已保存到 " + "F盘")
     else:
         print("下载失败：" + str(status))
+
 
 def parseFor(name):
     url = "http://h.syasn.com/?n=" + name + "&p=222222222"
@@ -47,9 +56,9 @@ def parseFor(name):
 if __name__ == "__main__":
     flag = True
     while flag:
-        print("=========== 下载选项 ===========")
-        category = input("\n 视频类别是：") # 如分类地址  http://v.23c.im/rs 填写 rs
-        name = input("\n 视频名称是：") # 如视频地址 http://v.23c.im/rs148 填写 rs148
+        print("\n=========== 下载选项(quit退出) ===========")
+        category = input("\n视频类别是：")  # 如分类地址  http://v.23c.im/rs 填写 rs
+        name = input("\n视频名称是：")  # 如视频地址 http://v.23c.im/rs148 填写 rs148
 
         baseVideoUrl = "http://k.syasn.com/" + category + "/"
 
