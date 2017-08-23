66a29235764052920addf26442f7c31b2ef0ed91
deeper <deeper@gmail.com>
2017-08-23 17:08:02 +0800

    update

--- download_crx.py
+++ download_crx.py
@@ -14,7 +14,7 @@ class UrlManager():
         # jstr = "{'name':'xiaoming','age':'22'}" // 错误，按照上面的写法
 
         # jdata = json.dumps(jstr) # 把python字典转为json字符串
-        jsonObject = json.loads(response.content)  # 把json字符串转为python字典
+        jsonObject = json.loads(response.content.decode("utf-8"))  # 把json字符串转为python字典
         _list = jsonObject["list"]
         for o in _list:
             name = o["name"]
@@ -23,15 +23,18 @@ class UrlManager():
             descpic = o["descpic"] # 图片
             self.download(name,crxUrl)
     def download(self,name,url):
-        time.sleep(3)  # 3s
+        time.sleep(0.2)  # 0.2s
         print("正在下载 == 》 "+ name)
         prefix = name + "_"
         headers = {'User-Agent': 'Baidu Spidder','Referer':'http://www.baidu.com'}
         response = requests.get(url,headers=headers)
         filename = url.split("/")[-1]
         dest = "F:/" + prefix + filename
-        with open(dest,"wb") as fout:
-        	fout.write(response.content)
+        try:
+            with open(dest,"wb") as fout:
+                fout.write(response.content)
+        except Exception as e:
+            print(dest + " 发生异常")
         print("《===="+name+" 下载完成====》")
 
 class Demo():
@@ -41,7 +44,7 @@ class Demo():
 
     def craw(self):
         print(" begin")
-        for u in range(1,2):
+        for u in range(5,10):
             self.urlManager.getPage(u)
 
 if __name__ == "__main__":
